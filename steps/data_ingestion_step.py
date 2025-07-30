f0d7f60afa22e2f16cfc5875adbdd99994c1d773
Himanshu <himanshujangir1601@gmail.com>
2025-07-30 14:46:13 +0530

    added data ingesstion step and handling missing values step.

---
+++ steps/data_ingestion_step.py
@@ -0,0 +1,16 @@
+from source.ingest_data import DataIngestorFactory
+from zenml import step
+import pandas as pd
+
+@step
+def data_ingestion_step(file_path: str) -> pd.DataFrame:
+    """Ingest data from a ZIP file using the appropriate DataIngestor."""
+    # Determine the file extension.
+    file_extension = ".zip" # Since we're dealing with ZIP files, this is hardcoded
+
+    # Get the appropriate DataIngestor
+    data_ingestor = DataIngestorFactory.get_data_ingestor(file_extension)
+
+    # Ingest the data and load it into a DataFrame
+    df = data_ingestor.ingest(file_path)
+    return df
---
+++ steps/handle_missing_value_step.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from source.handle_missing_values import DropMissingValuesStrategy, FillingMissingValuesStrategy,MissingValueHandler
+from zenml import step
+
+@step
+def handle_missing_values_step(df: pd.DataFrame, strategy: str = "mean") -> pd.DataFrame:
+    """Handles missing values using MissingValueHandler and the specified strategy."""
+    if strategy == "drop":
+        handler = MissingValueHandler(DropMissingValuesStrategy(axis=0))
+    elif strategy in ["mean", "median", "mode", "constant"]:
+        handler = MissingValueHandler(FillingMissingValuesStrategy(method=strategy))
+    else:
+        raise ValueError(f"Unsupported missing value handling strategy {strategy}.")
+    
+    cleaned_df = handler.handler(df)
+    return cleaned_df
